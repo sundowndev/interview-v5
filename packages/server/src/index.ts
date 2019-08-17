51f73f2e6d9386fe76eb204eee8b9d730cce79a8
sundowndev <raphael@crvx.fr>
2019-08-17 10:47:23 +0200

    refactor(api): useUnifiedTopology mongodb client option

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -69,6 +69,7 @@ async function main() {
       synchronize: false,
       logging: false,
       useNewUrlParser: true,
+      validateOptions: { useUnifiedTopology: true },
     });
 
     // ---------- SERVER ----------
